ea5dcd84cdb317aae16413260b9151ea7ddba6db
IYDoe <yudin2401@gmail.com>
2021-09-19 14:38:46 +0300

    last update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
-import { Navbar, Homepage, Exchanges, Cryptocurencies, CryptoDetails, News } from './components';
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components';
 import './App.css';
 
 const App = () => {
@@ -21,8 +21,8 @@ const App = () => {
               <Route exact path="/exchanges">
                 <Exchanges />
               </Route>
-              <Route exact path="/cryptocurencies">
-                <Cryptocurencies />
+              <Route exact path="/cryptocurrencies">
+                <Cryptocurrencies />
               </Route>
               <Route exact path="/crypto/:coinId">
                 <CryptoDetails />
--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import 'antd/dist/antd.css';
+import store from './app/store';
+import { Provider } from 'react-redux';
 
 reactDom.render(
   <Router>
+    <Provider store={store}>
       <App/>
+    </Provider> 
   </Router>, 
   document.getElementById('root')
 );
